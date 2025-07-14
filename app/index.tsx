490c9ba893daa828c452f516c52fd6db53899736
Ravi <raviyadav5951@gmail.com>
2025-07-14 21:56:08 +0400

    initial push

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native"
+import "./globals.css"
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-5xl text-center text-blue-500 font-quicksand-bold">
+        Welcome to RN!
+      </Text>
+    </View>
+  )
+}
